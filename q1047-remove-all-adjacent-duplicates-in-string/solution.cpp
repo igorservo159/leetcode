42e666ab0ccd4e7070d3e08ba00e1ab077c21a1e
Joao Igor Ramos de Lima <igorservo159@gmail.com>
2026-06-16 12:10:42 -0300

    Add solution for 1047 - remove all adjacent duplicates in string

---
+++ q1047-remove-all-adjacent-duplicates-in-string/solution.cpp
@@ -0,0 +1,16 @@
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+  string removeDuplicates(string s) {
+    string stack;
+    for (char c : s) {
+      if (!stack.empty() && stack.back() == c)
+        stack.pop_back();
+      else
+        stack.push_back(c);
+    }
+    return stack;
+  }
+};
