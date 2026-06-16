2f01f6cb9cf81dd2ec3c60e78c52cfab7f8ca883
Joao Igor Ramos de Lima <igorservo159@gmail.com>
2026-06-16 12:10:42 -0300

    Add solution for 160 - intersection of two linked lists

---
+++ q0160-intersection-of-two-linked-lists/solution.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+  ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+    ListNode *nodeA = headA, *nodeB = headB;
+
+    while (nodeA != nodeB) {
+      nodeA = nodeA ? nodeA->next : headB;
+      nodeB = nodeB ? nodeB->next : headA;
+    }
+
+    return nodeA;
+  }
+};
